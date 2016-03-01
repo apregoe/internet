3614906e2d9f0b0e5f1fe42ded397543019f0830
Albert Prego <prego@usc.edu>
2016-03-01 09:07:44 -0800

    basic sniffing done

--- assgn2/balancer.cpp
+++ assgn2/balancer.cpp
@@ -1,10 +1,20 @@
+#include "sniff.h"// taken from http://www.tcpdump.org/pcap.html
+
 #include <iostream>
 #include <string>
+
 #include <pcap.h>
 
+#include <arpa/inet.h>
+
+//compile-time definitions
+/* ethernet headers are always exactly 14 bytes [1] */
+#define SIZE_ETHERNET 14
 
 //Global variables
-
+/*
+sudo ./balancer -r pcapSamples/udp-multipleIP-6.pcap -i en0 -l logfile.txt -p -b
+*/
 //user input
 std::string filename, logfile, interface;
 bool p, b, s, d;//flags
@@ -13,21 +23,103 @@ bool p, b, s, d;//flags
 //func signatures
 bool parseInput(int argv, char** argc);//parses input
 void printParsedResults();//prints results
+void got_packet(const pcap_pkthdr *header, const u_char *packet);
 
 int main(int argv, char** argc) {
     p = b = s = d = false;
+    char errBuff[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = NULL;
+    bpf_program fp;//compiled version of our filter
+    bpf_u_int32 mask;//The netmask of our sniffing device
+    bpf_u_int32 net;//The IP of our sniffing device
+    pcap_pkthdr header;//header of the received packet
+    const u_char* packet = NULL;//packet received
 
-    if(parseInput(argv, argc)){
-        std::cout << "usage: balancer [-r filename] [-i interface] [ -l filename ] " << 
-                        "[-p] [-b] [-s] [-d] \n or \n" << 
+    if(!parseInput(argv, argc)){
+        std::cout << "usage: ./balancer [-r filename] [-i interface] [ -l filename ] " <<
+                        "[-p] [-b] [-s] [-d] \n or \n" <<
                         "./balancer [-r filename] [-i interface] " <<
                         "[-w num] [ -l filename ] [-c configpercent]" << std::endl;
         return 1;
     }
 
+    //getting network from mask and interface
+    if(pcap_lookupnet(interface.c_str(), &net, &mask, errBuff) == -1){
+        std::cerr << "Problem in funct pcap_lookupnet: " << errBuff << std::endl << strerror(errno) << std::endl;
+        return 1;
+    }
 
+    //opening pcap in which we will sniff
+    if((handle = pcap_open_live(interface.c_str(), BUFSIZ, 1, 1000, errBuff)) == NULL){
+        std::cerr << "Problem in funct pcap_open_live: " << errBuff << std::endl << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    //verifying correct data link
+    if(pcap_datalink(handle) != DLT_EN10MB){
+        std::cerr << "Problem in funct pcap_datalink: " << errBuff << std::endl << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    //compile version
+    if(pcap_compile(handle, &fp, "ip", 0, net) == -1){
+        std::cerr << "Problem in funct pcap_compile: " << errBuff << std::endl << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    //seting up filter
+    /*if (pcap_setfilter(handle, &fp) == -1) {
+        std::cerr << "Problem in funct pcap_setfilter: " << errBuff << std::endl << strerror(errno) << std::endl;
+        return 1;
+    }*/
+
+    //looping through every packet the sniff device gets
+    //get_packet is the call back function
+    while(true){
+        packet = pcap_next(handle, &header);
+        got_packet(&header, packet);
+    }
+    pcap_close(handle);
 
     return 0;
+}
+
+void got_packet(const pcap_pkthdr *header, const u_char *packet){
+
+    const sniff_ethernet *ethernet; /* The ethernet header */
+    const sniff_ip *ip; /* The IP header */
+    const sniff_tcp *tcp; /* The TCP header */
+    const char *payload; /* Packet payload */
+
+    u_int size_ip;
+    u_int size_tcp;
+
+    if(packet == NULL){
+        std::cout << "returning" << std::endl;
+        return;
+    }
+
+    //casting the packet
+    ethernet = (sniff_ethernet*)packet;
+    ip = (sniff_ip *)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+        //std::cerr << "Problem in funct get_packet, out in size_ip. \n invalid size_ip.";
+        return;
+    }
+    tcp = (sniff_tcp *)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20) {
+        //std::cerr << "Problem in funct get_packet, out in size_tcp. \n invalid size_tcp.";
+        return;
+    }
+    payload = (char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+
+
+    std::cout << "From: " << inet_ntoa(ip->ip_src) << std::endl;
+    std::cout << "To:   " << inet_ntoa(ip->ip_dst) << std::endl;
+    std::cout << "Size: " << header->len << std:: endl << std::endl;
+
 }
 
 bool parseInput(int argv, char** argc){
@@ -36,6 +128,12 @@ bool parseInput(int argv, char** argc){
             std::cout << *(argc+i) << " " << std::endl;
         }
     #endif
+
+    if(argv == 1){
+        return false;
+    }
+
+    int flagsCount = 0;
 
 
     #if DEBUGPARSE
@@ -72,32 +170,40 @@ bool parseInput(int argv, char** argc){
                 continue;
             }else if(strcmp(argc[i], "-p") == 0){
                 p = true;
+                ++flagsCount;
                 #if DEBUGPARSE
                     std::cout << "-p = true" << argc[i] << std:: endl;
                 #endif
                 continue;
             }else if(strcmp(argc[i], "-b") == 0){
                 b = true;
+                ++flagsCount;
                 #if DEBUGPARSE
                     std::cout << "-b = true" << argc[i] << std:: endl;
                 #endif
                 continue;
             }else if(strcmp(argc[i], "-s") == 0){
                 s = true;
+                ++flagsCount;
                 #if DEBUGPARSE
                     std::cout << "-s = true: " << std:: endl;
                 #endif
                 continue;
-            }else if (strcmp(argc[i], "-d")){
+            }else if (strcmp(argc[i], "-d") == 0){
                 d = true;
+                ++flagsCount;
                 #if DEBUGPARSE
                     std::cout << "-d = true" << argc[i] << std:: endl;
                 #endif
                 continue;
+            }else{
+                std::cout << "unknown command: " << argc[i] << std::endl;
+                return false;
             }
+        }
+        if(flagsCount == 0){
             return false;
         }
-        
         #if DEBUGPARSE
             printParsedResults();
         #endif
@@ -114,19 +220,3 @@ void printParsedResults(){
     std::cout << "s = " << s << std::endl;
     std::cout << "d = " << d << std::endl;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
